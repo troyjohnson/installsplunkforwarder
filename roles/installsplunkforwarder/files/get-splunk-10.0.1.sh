b57b4957ad3ed7f7df1d3b89a7a80feea2c7e2d4
Troy Johnson <troy@jdmz.net>
2025-10-28 20:54:21 +0000

    updated for version 10.0.1 and a get installables script

---
+++ roles/installsplunkforwarder/files/get-splunk-10.0.1.sh
@@ -0,0 +1,12 @@
+#! /bin/bash
+
+# get-splunk-10.0.1.sh
+
+wget -O splunk-10.0.1-c486717c322b-linux-amd64.deb "https://download.splunk.com/products/splunk/releases/10.0.1/linux/splunk-10.0.1-c486717c322b-linux-amd64.deb"
+
+wget -O splunk-10.0.1-c486717c322b.x86_64.rpm "https://download.splunk.com/products/splunk/releases/10.0.1/linux/splunk-10.0.1-c486717c322b.x86_64.rpm"
+
+wget -O splunkforwarder-10.0.1-c486717c322b-linux-amd64.deb "https://download.splunk.com/products/universalforwarder/releases/10.0.1/linux/splunkforwarder-10.0.1-c486717c322b-linux-amd64.deb"
+
+wget -O splunkforwarder-10.0.1-c486717c322b.x86_64.rpm "https://download.splunk.com/products/universalforwarder/releases/10.0.1/linux/splunkforwarder-10.0.1-c486717c322b.x86_64.rpm"
+
